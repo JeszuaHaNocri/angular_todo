87244b886000906ee0c18c6143cfaa662c9b7423
JeszuaHaNocri <lachtaraweronika@gmail.com>
2015-01-02 00:20:02 +0000

    add todo list

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ var app = angular.module('Todo', []);
 
 app.controller('TodoCtrl', function ($scope) {
     $scope.message = 'Message';
+    $scope.todos = [
+        'Learn Sketch',
+        'Look at Dribbble and feel inferior',
+        'Actually learn how to use the Pen tool'
+    ];
 
 });
